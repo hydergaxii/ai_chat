e586533bb814ebf19ffcd1fce3f7d0f232dc8959
hydergaxii <hydergaxi.hg01@gmail.com>
2026-03-25 04:39:36 +0000

    Fix widget test - use AiChatApp not MyApp

---
+++ test/widget_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ai_chat/main.dart';
+
+void main() {
+  testWidgets('App renders without crashing', (WidgetTester tester) async {
+    await tester.pumpWidget(const ProviderScope(child: AiChatApp()));
+    expect(find.byType(MaterialApp), findsOneWidget);
+  });
+}
